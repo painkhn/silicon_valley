cacad46f1e228b9c84d2b35908e8d7a0f827271e
painkhn <1fotice@gmail.com>
2024-11-27 23:11:09 +0800

    project upload

---
+++ src/script.js
@@ -0,0 +1,6 @@
+const searchToggle = () => {
+    const searchToggleButton = document.querySelector('.searchToggleButton')
+    const searchInput = document.querySelector('.searchInput')
+
+    searchInput.classList.toggle('hidden')
+}
