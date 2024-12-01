16a1ef836342ab2f0337b0e80cf27aa9a30d9893
1001001010 <lll100100@yandex.ru>
2024-12-01 13:23:20 +0800

    Убрал фронт

--- src/script.js
+++
@@ -1,6 +0,0 @@
-const searchToggle = () => {
-    const searchToggleButton = document.querySelector('.searchToggleButton')
-    const searchInput = document.querySelector('.searchInput')
-
-    searchInput.classList.toggle('hidden')
-}
